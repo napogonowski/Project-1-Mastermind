05316a524bd709d0b52c0e62a65e472d4e3a00b0
pxner0 <napogonowski@outlook.com>
2023-07-10 22:32:10 +1000

    player selection added

--- js/main.js
+++ js/main.js
@@ -7,33 +7,112 @@ const COLORS = {
     '4': "#c40fd1", 
     '5': "#FF7415"
 }
-const LEVELS ={
-    '0': '12',
-    '1': '9',
-    '2': '6'
-} 
+const PEGCOLORS = {
+    "0": 'grey',
+    '1': 'white',
+    '2': 'red'
+}
+/// probably wont use this 
+// const LEVELS ={
+//     '0': '12',
+//     '1': '9',
+//     '2': '6'
+//} 
 
 /*----- state variables -----*/
 let board; // player choices per turn 
 let pegs; // indicating if the choices are included in the secret code 
+let currentRow; 
 let win; // 4 positions 
 let secretCode; // code chosen by random computer 
 
 
 /*----- cached elements  -----*/
-
+const marbelRows = document.querySelectorAll("div.marbles > div");
+const colorSquares = document.querySelectorAll("div.color-squares");
+const clearBtn = document.querySelector("#clear");
+const submitBtn = document.querySelector("#submit");
 
 /*----- event listeners -----*/
-// 1. for color selection 
+// submitting selection
+submitBtn.addEventListener("click", checkChoice)
+//clearing seleciton 
+clearBtn.addEventListener("click", clearChoice)
 
-// 1.a using color picked to select designate color combo 
 
-//2. submit button 
-
-
-[1, 2, 3, 4 ]
-
-[0, 2, 4, 2]
-
+colorSquares.forEach(function (colorSquare, colorIndex) {
+    colorSquare.addEventListener("click", (event) => addColor(event, colorIndex))
+}); 
+    
 
 /*----- functions -----*/
+init();
+// initialise all state, then call render; 
+function init() {
+    board = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    ]; 
+
+    pegs = [];
+// index of starting row 
+    currentRow = 0;
+    win = false; 
+    secretCode = [
+        Math.floor(Math.random()* 6),
+        Math.floor(Math.random()* 6),
+        Math.floor(Math.random()* 6),
+        Math.floor(Math.random()* 6)
+    ];
+    console.log(secretCode);
+    render();
+}
+
+function render() {
+    console.log(board[0]);
+    board[0].forEach((value, idx) => {
+        // update the state
+        // array has new value. need to represent it visually 
+        // nested for each first goes over the array, then loop through the elements of the array/ 
+
+       marbelRows[idx].style.backgroundColor = COLORS[value];
+       
+    })
+};
+
+
+
+
+
+
+
+function checkChoice(e){
+
+};
+
+// ioncrement current row ++; 
+// when clicked, the users code choice is checked 
+
+
+function addColor(e, colorIndex){
+    if(board[0].length >=4) return; 
+  board[0].push(colorIndex);
+  render(); 
+};
+    
+// when the user clicks a color selector
+// the color chosen should fill the firrst available row cell 
+
+function clearChoice(e){
+
+};
+
+// when clicked, the function should clear all previously filled marble cells. 
