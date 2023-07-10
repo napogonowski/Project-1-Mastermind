848d5ede82f19e21deaded53121ed7fb7c8fc11a
pxner0 <napogonowski@outlook.com>
2023-07-10 12:10:37 +1000

    inital board and grid configuration

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*----- constants -----*/
+const COLORS = {
+    '0': "#D30000",
+    '1': "#0a53c9",
+    '2': "#09b009",
+    '3': "#FFF200",
+    '4': "#c40fd1", 
+    '5': "#FF7415"
+}
+const LEVELS ={
+    '0': '12',
+    '1': '9',
+    '2': '6'
+} 
+
+/*----- state variables -----*/
+let board; // player choices per turn 
+let pegs; // indicating if the choices are included in the secret code 
+let win; // 4 positions 
+let secretCode; // code chosen by random computer 
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+// 1. for color selection 
+
+// 1.a using color picked to select designate color combo 
+
+//2. submit button 
+
+
+[1, 2, 3, 4 ]
+
+[0, 2, 4, 2]
+
+
+/*----- functions -----*/
