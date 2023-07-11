50c526531981af9be69acd3de350169adb97789f
pxner0 <napogonowski@outlook.com>
2023-07-11 16:13:11 +1000

    full board,  color selector working on all rows

--- js/main.js
+++ js/main.js
@@ -8,10 +8,11 @@ const COLORS = {
     '4': "#c40fd1", 
     '5': "#FF7415"
 }
+
 const PEGCOLORS = {
-    "0": 'grey',
+    "-1": 'lightgray', // think this is irellevant
     '1': 'white',
-    '2': 'red'
+    '2': 'black'
 }
 /// probably wont use this 
 // const LEVELS ={
@@ -29,10 +30,11 @@ let secretCode; // code chosen by random computer
 
 
 /*----- cached elements  -----*/
-const marbelRows = document.querySelectorAll("div.marbles > div");
-const colorSquares = document.querySelectorAll("div.color-squares");
 const clearBtn = document.querySelector("#clear");
 const submitBtn = document.querySelector("#submit");
+//const marbelCells = document.querySelectorAll("div.marbles > div");
+const colorSquares = document.querySelectorAll("div.color-squares");
+const pegCells = document.querySelectorAll("div.pegs > div");
 
 /*----- event listeners -----*/
 // submitting selection
@@ -63,7 +65,18 @@ function init() {
         [],
     ]; 
 
-    pegs = [];
+    pegs = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    ];
 // index of starting row 
     currentRow = 0;
     win = false; 
@@ -76,35 +89,44 @@ function init() {
     //console.log(secretCode);
     render();
 }
+// board[currentRow].forEach((value, idx) => {    
+//     marbelCells[idx * currentRow].style.backgroundColor = COLORS[value];
+// })
 
 function render() {
-    board[0].forEach((value, idx) => {    
-    marbelRows[idx].style.backgroundColor = COLORS[value];
+    board.forEach(function (rowArr, rowIdx){
+        rowArr.forEach(function (cellVal, colIdx){
+            const cellId = `r${rowIdx}m${colIdx}`;
+            const cellEl = document.getElementById(cellId);
+            cellEl.style.backgroundColor = COLORS[cellVal]
+
+        
+            //cellEl.style.backgroundColor = COLORS[cellVal]
+        })
     })
 };
 
-function checkChoice(e){
+ function checkChoice(e){
 
-};
+ };
 
-// ioncrement current row ++; 
-// when clicked, the users code choice is checked 
+// essentially winning logic 
+// we want to compare arrays to secret arrays 
+// if === win = true 
+// if correct vals (colors)/ in wrong spot = 1+s 
+// if correct val in correct idx = 2 
+// then push that array and render the pegs 
 
 
 function addColor(e, colorIndex){
-    if(board[0].length >=4) return; 
-  board[0].push(colorIndex);
-  render(); 
+    if(board[currentRow].length >=4) return; 
+    board[currentRow].push(colorIndex);
+    render(); 
 };
     
-// when the user clicks a color selector
-// the color chosen should fill the firrst available row cell 
-
 function clearChoice(e){
-   board[0] = [-1, -1, -1, -1];
+   board[currentRow] = [-1, -1, -1, -1];
    render();
-   board[0] = [];
+   board[currentRow] = [];
 };
 
-// when clicked, the function should clear all previously filled marble cells. 
-// any time state is changed; call reneder 
